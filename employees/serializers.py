cdd6ba62ce6248649d8da412b83f142ed80581c9
mayank kumar <80468358+1jb12me030@users.noreply.github.com>
2021-10-21 16:09:34 +0530

    Initial commit

---
+++ employees/serializers.py
@@ -0,0 +1,8 @@
+from django.db.models import fields
+from rest_framework import serializers
+from . models import employees
+class employeesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = employees
+#        fields = ('firstname', 'lastname')
+    fields ='__all__'
